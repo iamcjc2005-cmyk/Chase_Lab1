a382cba96867aa48a532d78091eba33082334e61
cjc2005 <iamcjc2005@gmail.com>
2026-08-17 15:29:55 -0500

    Added comment via GitHub website
    Updated comment to reflect direct editing on GitHub.

--- Chase_Lab1/Program.cs
+++ Chase_Lab1/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Edited directly on Github
 Console.WriteLine("Hello from Chase! This is my first GIT commit.");
 Console.WriteLine("Git makes collaboration easy!");
