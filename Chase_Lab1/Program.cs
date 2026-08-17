24efb7f609399a6ebacd0c52c587691260a5b53a
CJ Chisholm <iamcjc2005@gmail.com>
2026-08-17 15:04:33 -0500

    Added second output line

--- Chase_Lab1/Program.cs
+++ Chase_Lab1/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello from Chase! This is my first GIT commit.");
+Console.WriteLine("Git makes collaboration easy!");
