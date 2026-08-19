b14d09b7b5d3f6d5a6179754101bac0a8d6fdee6
1tylerhhighland@gmail.com <1tylerhhighland@gmail.com>
2026-08-19 14:30:43 -0500

    Reviewed and updated by Tyler

--- Chase_Lab1/Program.cs
+++ Chase_Lab1/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello from Chase! This is my first GIT commit.");
 Console.WriteLine("Git makes collaboration easy!");
 Console.WriteLine("edited by tyler");
+Console.WriteLine("Reviewed and updated by Tyler");
