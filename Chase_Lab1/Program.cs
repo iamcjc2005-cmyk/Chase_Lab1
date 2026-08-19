1546200c89e7abd62a1d0bf295cce0bb9bc36f3f
1tylerhhighland@gmail.com <1tylerhhighland@gmail.com>
2026-08-19 14:21:01 -0500

    Changes made by Tyler

--- Chase_Lab1/Program.cs
+++ Chase_Lab1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// Edited directly on Github
 Console.WriteLine("Hello from Chase! This is my first GIT commit.");
 Console.WriteLine("Git makes collaboration easy!");
+Console.WriteLine("edited by tyler");
