7451e714674d0268332ef35695f90af291c1ba7d
CJ Chisholm <iamcjc2005@gmail.com>
2026-08-19 15:00:48 -0500

    Final Changes

--- Chase_Lab1/Program.cs
+++ Chase_Lab1/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello from Chase! This is my first GIT commit.");
 Console.WriteLine("Git makes collaboration easy!");
 Console.WriteLine("edited by tyler");
 Console.WriteLine("Reviewed and updated by Tyler");
+Console.WriteLine("Lab 1 completed on 08/19/26");
