c5304236d9bd4bd7bb368a4b64fae3bcb99f3d71
1tylerhhighland@gmail.com <1tylerhhighland@gmail.com>
2026-08-24 14:18:00 -0500

    Tyler changes greeting

--- Chase_Lab1/Program.cs
+++ Chase_Lab1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Edited directly on Github
-Console.WriteLine("Hello from Chase! This is my first GIT commit.");
+Console.WriteLine("Hello from Tyler");
 Console.WriteLine("Git makes collaboration easy!");
 Console.WriteLine("edited by tyler");
 Console.WriteLine("Reviewed and updated by Tyler");
